eea39e7ea37450262c8182f0e33e426aa269f428
Igor Kurek <igorkurek96@gmail.com>
2018-05-21 21:18:03 +0200

    [WIP]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :answers
-  resources :questions
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :questions do
+    resources :answers
+  end
 end
